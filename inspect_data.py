a3ae2f62f0158ce6ce5c701efba6fd38ee1ee8a2
Parviz <miyagy144@gmail.com>
2026-08-15 13:00:31 +0300

    Visualize data with plt

--- inspect_data.py
+++ inspect_data.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from data import load_mnist
 
 x_train, y_train, x_test, y_test = load_mnist()
@@ -9,3 +10,10 @@ print("y_test: ", y_test.shape)
 
 print("Pixel range:", x_train.min(), x_train.max())
 print("First label:", y_train[0])
+
+first_image = x_train[0].reshape(28, 28)
+
+plt.imshow(first_image, cmap="gray")
+plt.title(f"Label: {y_train[0]}")
+plt.axis("off")
+plt.show()
